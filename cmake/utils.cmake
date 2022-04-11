38f197e653577c35be7ae92b9397472a575006d4
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2022-04-11 13:55:03 +0200

    Include subarchitecture and float handling info in package name
    The package name shall include all the relevant info, to find out what
    does it contain. Thus, we include the subarchitecture name (cortex-m*)
    and the floating point unit, if applicable, or 'soft_float' if not, in
    the package name. We retrieve it from the target compile flags, from the
    LLVM_BAREMETAL_ARM_TARGET_COMPILE_FLAGS  variable. The info, whether
    exceptions are enabled for that build is also included.

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -7,7 +7,7 @@ function(ConfigurePackaging)
     set(CPACK_PACKAGE_VERSION_PATCH ${PROJECT_VERSION_PATCH})
     set(CPACK_VERBATIM_VARIABLES YES)
     set(CPACK_GENERATOR TGZ)
-    set(CPACK_SYSTEM_NAME armv7em)
+    set(CPACK_SYSTEM_NAME arm)
     set(CPACK_INCLUDE_TOPLEVEL_DIRECTORY OFF)
 
     set(version ${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH})
@@ -20,11 +20,58 @@ endfunction()
 
 function(AddPackagingTarget)
 
+    GetSubarchitectureAndFpu(sub_and_fpu)
+
+    if(LLVM_BAREMETAL_ARM_ENABLE_EXCEPTIONS)
+        set(exceptions_str "-exceptions")
+    else()
+        set(exceptions_str "-no_exceptions")
+    endif()
+
     # It assumes that the default 'cpack' invocation will create a *.tar.gz archive, with the name 
     # ${CPACK_PACKAGE_FILE_NAME}.tar.gz in the current directory, according to the config from ConfigurePackaging().
+
+    # We could use the CPACK_POST_BUILD_SCRIPT to accomplish the renaming, but in multi-config builds, there is no
+    # way to determine the current config, to append it to the name.
     add_custom_target(pack 
         COMMAND ${CMAKE_CPACK_COMMAND} -C $<CONFIG>
-        COMMAND ${CMAKE_COMMAND} -E copy ${CPACK_PACKAGE_FILE_NAME}.tar.gz ${CPACK_PACKAGE_FILE_NAME}-$<CONFIG>.tar.gz
+        COMMAND ${CMAKE_COMMAND} -E copy 
+            ${CPACK_PACKAGE_FILE_NAME}.tar.gz
+            ${CPACK_PACKAGE_FILE_NAME}-${sub_and_fpu}${exceptions_str}-$<CONFIG>.tar.gz
     )
+
+endfunction()
+
+
+function(GetSubarchitectureAndFpu result_out_var)
+
+    string(FIND ${LLVM_BAREMETAL_ARM_TARGET_COMPILE_FLAGS} "cortex" cpu_begin)
+    if(cpu_begin LESS 0)
+        set(${result_out_var} "sub_unknown" PARENT_SCOPE)
+        return()
+    endif()
+
+    # Get 'cortex-*' string from the compiler flags.
+    string(SUBSTRING ${LLVM_BAREMETAL_ARM_TARGET_COMPILE_FLAGS} ${cpu_begin} -1 cpu)
+    string(FIND ${cpu} " " cpu_end)
+    string(SUBSTRING ${cpu} 0 ${cpu_end} cpu)
+
+    string(FIND ${LLVM_BAREMETAL_ARM_TARGET_COMPILE_FLAGS} "fpu" fpu_begin)
+    if(fpu_begin LESS 0)
+        set(${result_out_var} "${cpu}-soft_float" PARENT_SCOPE)
+        return()
+    endif()
+
+    # Set index after "fpu=" string
+    math(EXPR fpu_begin "${fpu_begin} + 4" OUTPUT_FORMAT DECIMAL)
+    string(SUBSTRING ${LLVM_BAREMETAL_ARM_TARGET_COMPILE_FLAGS} ${fpu_begin} -1 fpu)
+
+    # '-mfpu' might not be at the end of the sting with the compilation flags.
+    string(FIND ${fpu} " " fpu_end)
+    if(fpu_end GREATER 0)
+        string(SUBSTRING ${fpu} 0 ${fpu_end} fpu)
+    endif()
+
+    set(${result_out_var} "${cpu}-${fpu}" PARENT_SCOPE)
 
 endfunction()
